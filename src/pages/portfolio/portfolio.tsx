9eba7ac1c3c969196b16c7e5db1d7abbaa6bc224
Josh Roth <raider4414@gmail.com>
2025-01-24 00:27:28 -0600

    landing page updated

--- src/pages/portfolio/portfolio.tsx
+++ src/pages/portfolio/portfolio.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './portfolio.css'; 
 import Iphone from '../../assets/iphone.svg'
 import Ipad from '../../assets/ipad.svg'
@@ -14,6 +14,17 @@ const Portfolio = () => {
     const [selectedProject, setSelectedProject] = useState<string | null>(null);
     const [modalSize, setModalSize] = useState({ width: '80%', height: '80vh' });
 
+    useEffect(() => {
+        if (selectedProject) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, [selectedProject]);
+
     const projects = [
         {
             name: 'Bullies of Texas',
@@ -22,38 +33,35 @@ const Portfolio = () => {
             image: Bullies,
         },
         {
-         name: 'Valwood Park Church',
-         description: 'A Church Website',
-         url: 'https://www.valwoodparkchurch.com',
-         image: Valwood,
-     },     
-     {
-      name: 'Purpose and Peace',
-      description: 'A Christian Counseling site',
-      url: 'https://www.purposeandpeace.com',
-      image: Purpose,
-  },
+            name: 'Valwood Park Church',
+            description: 'A Church Website',
+            url: 'https://www.valwoodparkchurch.com',
+            image: Valwood,
+        },
+        {
+            name: 'Purpose and Peace',
+            description: 'A Christian Counseling site',
+            url: 'https://www.purposeandpeace.com',
+            image: Purpose,
+        },
         {
             name: 'Antioch Baptist Church',
             description: 'A church Website',
             url: 'https://www.abclovelady.com',
             image: Antioch,
         },
-
         {
-         name: 'Fourth Dimension',
-         description: 'An inormation site for products',
-         url: 'https://4di-inc.com',
-         image: Four,
-     },
-
-     {
-      name: 'Lakeview Baptist Church',
-      description: 'A Church Website',
-      url: 'https://www.lakeviewmbc.org',
-      image: Lake,
-  },
-        // Add more projects as needed
+            name: 'Fourth Dimension',
+            description: 'An information site for products',
+            url: 'https://4di-inc.com',
+            image: Four,
+        },
+        {
+            name: 'Lakeview Baptist Church',
+            description: 'A Church Website',
+            url: 'https://www.lakeviewmbc.org',
+            image: Lake,
+        },
     ];
 
     const openModalOrWindow = (url: string) => {
@@ -77,11 +85,7 @@ const Portfolio = () => {
             <h1>Portfolio</h1>
             <p>Here are some of my projects:</p>
             <p>
-            In web design, 
-            the ultimate goal is to create a product 
-            that satisfies the client's vision while ensuring usability, 
-            functionality, and aesthetic appeal. There are times when a client's 
-            preferences or ideas may not align with best practices or my design instincts. 
+                In web design, the ultimate goal is to create a product that satisfies the client's vision while ensuring usability, functionality, and aesthetic appeal. There are times when a client's preferences or ideas may not align with best practices or my design instincts.
             </p>
             <p>Please feel free to ask me about any design choices.</p>
             <ul>
@@ -97,27 +101,26 @@ const Portfolio = () => {
 
             {selectedProject && (
                 <div className="modal" onClick={closeModal}>
-                   <div className="resize-buttons">
-                            <button className='close' onClick={closeModal}>Exit</button>
-                            <button onClick={(e) => { e.stopPropagation(); resizeModal('390px', '844px'); }}>
-                                <img src={Iphone} alt="Phone Icon" className="button-icon" />
-                                Phone
-                            </button>
-                            <button onClick={(e) => { e.stopPropagation(); resizeModal('834px', '1194px'); }}>
-                                <img src={Ipad} alt="Tablet Icon" className="button-icon" />
-                                Tablet
-                            </button>
-                            <button onClick={(e) => { e.stopPropagation(); resizeModal('1280px', '720px'); }}>
-                            <img src={Mac} alt="Tablet Icon" className="button-icon" />
+                    <div className="resize-buttons">
+                        <button className='close' onClick={closeModal}>Exit</button>
+                        <button onClick={(e) => { e.stopPropagation(); resizeModal('390px', '844px'); }}>
+                            <img src={Iphone} alt="Phone Icon" className="button-icon" />
+                            Phone
+                        </button>
+                        <button onClick={(e) => { e.stopPropagation(); resizeModal('834px', '1194px'); }}>
+                            <img src={Ipad} alt="Tablet Icon" className="button-icon" />
                             Tablet
-                            </button>
-                        </div>
+                        </button>
+                        <button onClick={(e) => { e.stopPropagation(); resizeModal('1280px', '720px'); }}>
+                            <img src={Mac} alt="Desktop Icon" className="button-icon" />
+                            Desktop
+                        </button>
+                    </div>
                     <div
                         className="modal-content"
                         onClick={(e) => e.stopPropagation()}
-                        style={{ width: modalSize.width, height: modalSize.height }}
+                        style={{ width: modalSize.width, height: modalSize.height, overflow: 'auto' }}
                     >
-                      
                         <iframe
                             src={selectedProject}
                             title="Project Preview"
